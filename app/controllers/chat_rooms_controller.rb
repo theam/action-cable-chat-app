436d6b6b2029e202141ac403d16870f4fdc22932
juanjoman <j2rgez@gmail.com>
2020-07-02 16:22:42 +0200

    BOOST-759 Enable action cable and add consumers

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -5,6 +5,7 @@ class ChatRoomsController < ApplicationController
 
   def show
     @chat_room = ChatRoom.includes(:messages).find_by(id: params[:id])
+    @message = Message.new
   end
 
   def new
